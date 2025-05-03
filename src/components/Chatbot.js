d6e29a82801b3bf1a92b877521dc6a60bed10982
aady07 <103576677+aady07@users.noreply.github.com>
2025-05-03 20:24:15 +0530

    url change

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -14,7 +14,7 @@ const ChatbotFrame = styled.iframe`
 const Chatbot = () => {
     return (
         <ChatbotFrame
-            src="http://localhost:5176/chatbot-iframe?id=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZGFyc2guc2luZ2giLCJpc3MiOiJ2a2FpLWltcGFjdC5jb20iLCJyb2xlIjoiYWRtaW4iLCJyZWdpb24iOiJJTiIsImlhdCI6MTcxNDczODQwMCwiZXhwIjoxNzE0NzQyMDAwfQ.-e0iv4-QhHq9_3O_WrZYiH2rCbx6f4xnpE2UwZ-KS3bTLxBoY0N2StwQKBlauNHnl2xZtwIYf4cpiZKqbHDqVw"
+            src="https://miraista.com/chatbot-iframe?id=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZGFyc2guc2luZ2giLCJpc3MiOiJ2a2FpLWltcGFjdC5jb20iLCJyb2xlIjoiYWRtaW4iLCJyZWdpb24iOiJJTiIsImlhdCI6MTcxNDczODQwMCwiZXhwIjoxNzE0NzQyMDAwfQ.-e0iv4-QhHq9_3O_WrZYiH2rCbx6f4xnpE2UwZ-KS3bTLxBoY0N2StwQKBlauNHnl2xZtwIYf4cpiZKqbHDqVw"
             allow="microphone"
             title="Chatbot"
         />
